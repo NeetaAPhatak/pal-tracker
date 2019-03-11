c298289b47c773f300206a58b0b5fe50336225e0
NareshMuluguri <n_muluguri@dell.com>
2019-03-11 15:16:41 -0400

    Externalized configuration lab

---
+++ src/PalTracker/EnvController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PalTracker
+{
+    [Route("env")]
+    public class EnvController : ControllerBase
+    {
+        private readonly CloudFoundryInfo _cloudFoundryEnv;
+
+        [HttpGet]
+        public CloudFoundryInfo Get() => _cloudFoundryEnv;
+
+        public EnvController(CloudFoundryInfo cloudFoundryEnv)
+        {
+            _cloudFoundryEnv = cloudFoundryEnv;
+        }
+    }
+}
